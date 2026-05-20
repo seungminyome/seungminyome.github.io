697e368dac2454248033be91dfc60c2e1e818560
seungminyome <wab8131028@gmail.com>
2026-05-20 10:31:32 +0000

    Deploying to gh-pages from @ seungminyome/seungminyome.github.io@5fa643cd2bc0f0d0bd3de9626b888f97eb3cb03a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "M.S. student in Energy &amp; Petroleum Engineering at the University of Wyoming. Research in computational mechanics, OpenFOAM/solids4Foam simulations, and subsurface energy systems.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
